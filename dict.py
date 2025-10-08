1835355634164b2863f8566ee3ddb49dc9523fd2
mustafa00mnkj-creator <mustafa00mnkj@gmail.com>
2025-10-08 12:16:56 +0300

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,30 @@
+#mustafa omar munem              group c 
+
+
+
+
+dic = {
+    "username": "mustafa",
+    "pass": "12345678",
+    "name": "mustafa omar",
+    "age": 19,
+    "mail": "mustafa@mail.ru",
+}
+attempts3 = 3
+attempts = 0
+while attempts < attempts3:
+    User = input("Enter Your Username: ")
+    Pass = input("Enter Your Password: ")
+    
+    if User == dic["username"] and Pass == dic["pass"]:
+        print(f"Welcome {dic['name']}\nYour Age is {dic['age']}\nYour Mail is {dic['mail']}")
+        break
+    else:
+        attempts += 1
+        remaining = attempts3 - attempts
+        
+        if remaining > 0:
+            print(f"Username or Password Wrong — {remaining} attempt left\n")
+        else:
+            dic.clear()
+            print("Account locked")
